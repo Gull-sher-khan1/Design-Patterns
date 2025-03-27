e8d3f21db32515cee1ed281e5cb3752bfdb18f65
Gull Sher Khan <gullsherkhan1@gmail.com>
2025-03-27 23:31:19 +0000

    Single responsibility principle added

--- Design Patterns/Program.cs
+++ Design Patterns/Program.cs
@@ -1,4 +1,7 @@
-﻿Console.WriteLine("Hello, World!");
+﻿//using SingleResponsibilityPrinciple;
+//var p = new Person("John", 12);
+//var persistance = new Persistance(@"C:\objectfile.txt");
+//persistance.Save(p);
 
 // To stop the console from closing.
 Console.ReadKey();
---
+++ Design Patterns/SOLID/Single responsibility principle/SRP.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingleResponsibilityPrinciple
+{
+    // Person has the responsibility related to an object but 
+    // it is saved through different class that has the responsibility
+    // to save the object.
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public override string ToString() 
+        {
+            return $"{Name} is {Age} years old.";
+        }
+    }
+
+    public class Persistance
+    {
+        public string Filename { get; set; }
+
+        public Persistance(string filename)
+        {
+            Filename = filename;
+        }
+
+        public void Save(Object obj)
+        {
+            using (StreamWriter w = File.AppendText(Filename))
+            {
+                w.WriteLine(obj.ToString());
+            }
+        }
+    }
+}
