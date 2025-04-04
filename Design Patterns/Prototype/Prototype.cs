426db29498e88db8124222e36520d8ea0dc98d3d
Gull Sher Khan <gullsherkhan1@gmail.com>
2025-04-04 16:19:59 +0100

    Prototype design pattern implemented

---
+++ Design Patterns/Prototype/Prototype.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prototype
+{
+    public interface IPrototype<T>
+    {
+        public T DeepCopy();
+    }
+
+    public class Person:IPrototype<Person>
+    {
+        public List<string> Names { get; set; }
+        public Address Address { get; set; }
+
+        public Person(string firstName, string lastName, Address address) 
+        { 
+            Names = new List<string>{ firstName, lastName};
+            Address = address;
+        }
+
+        public Person DeepCopy()
+        {
+            return new Person(Names[0], Names[1], Address.DeepCopy());
+        }
+
+        public override string ToString()
+        {
+            return $"Name: {Names[0] + " " + Names[1]}, Address: {Address}";
+        }
+    }
+
+    public class Address: IPrototype<Address> 
+    {
+        public int HouseNumber { get; set; }
+        public string StreetName { get; set; }
+
+        public Address(int houseNumber, string streetName)
+        {
+            HouseNumber = houseNumber;
+            StreetName = streetName;
+        }
+
+        public Address DeepCopy() 
+        {
+            return new Address(HouseNumber, StreetName);
+        }
+
+        public override string ToString() 
+        {
+            return $"{HouseNumber}, {StreetName}";
+        }
+    }
+}
--- Design Patterns/obj/Debug/net6.0/Design Patterns.AssemblyInfo.cs
+++ Design Patterns/obj/Debug/net6.0/Design Patterns.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Design Patterns")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+610ce6f030a9cb63b8d06147b5e6f030fa392f9a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6410e77f888197e26278ad80c4cb692dcb5ca1f4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Design Patterns")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Design Patterns")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
